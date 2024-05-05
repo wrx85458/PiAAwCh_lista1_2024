7f90098abb675f2cc622001a2c48b650d3745e56
wrx85458 <152792629+wrx85458@users.noreply.github.com>
2024-05-05 11:57:36 +0000

    Change main.tf

--- test-terraform-workflow/main.tf
+++ test-terraform-workflow/main.tf
@@ -1,3 +1,9 @@
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+
 resource "local_file" "script" {
   content  = var.console_script
   filename = "${path.module}/script.js"
